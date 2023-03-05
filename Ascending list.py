15d00baddbb95f933e7e0993cbf413a2e01f5296
Amir Samaei <55365996+asamaei@users.noreply.github.com>
2023-03-04 22:09:31 -0500

    solved

--- Ascending list.py
+++ Ascending list.py
@@ -5,9 +5,13 @@ Date: March 4, 2023
 Description: This program contains solutions to practice problems for the CCPS 109 course at the University of Ryerson.
 """
 
+
 def is_ascending(items):
- if len(items) < 2: # return True  # An empty sequence or a sequence with one element is always ascending
-    return True
- for i in range(1, len(items)):
-     if items[i] <= items[i - 1]:#return False  # The sequence is not strictly ascending if any adjacent pair of elements violates the condition
- return True  # All adjacent pairs of elements satisfy the condition, so the sequence is strictly ascending
+    if len(items) < 2:  # Check if the length of the sequence is less than 2
+        return True  # If the sequence is empty or has only one element, it is always ascending
+
+    for i in range(1, len(items)):  # Iterate through the sequence starting from the second element
+        if items[i] <= items[i - 1]:  # Check if the current element is less than or equal to the previous element
+            return False  # If any adjacent pair of elements violates the ascending order, return False
+
+    return True  # If all adjacent pairs of elements satisfy the ascending order, return True
